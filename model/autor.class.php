ba5c2c6032ad09c7b64439a947250c4f9e70122a
ayoubdaw <ayoubbouyssfdaouan@iespaucasesnoves.cat>
2018-12-12 20:49:49 +0100

    Get , update , delete

--- model/autor.class.php
+++ model/autor.class.php
@@ -8,16 +8,13 @@ class Autor
     private $conn;       //connexió a la base de dades (PDO)
     private $resposta;   // resposta
     
-    public function __CONSTRUCT()
-    {
+    public function __CONSTRUCT(){
         $this->conn = Database::getInstance()->getConnection();      
         $this->resposta = new Resposta();
     }
     
-    public function getAll($orderby="id_aut")
-    {
-		try
-		{
+    public function getAll($orderby="id_aut"){
+		try{
 			$result = array();                        
 			$stm = $this->conn->prepare("SELECT id_aut,nom_aut,fk_nacionalitat FROM autors ORDER BY $orderby");
 			$stm->execute();
@@ -26,23 +23,32 @@ class Autor
 			$this->resposta->setCorrecta(true);       // La resposta es correcta        
             return $this->resposta;
 		}
-        catch(Exception $e)
-		{   // hi ha un error posam la resposta a fals i tornam missatge d'error
+        catch(Exception $e){   // hi ha un error posam la resposta a fals i tornam missatge d'error
 			$this->resposta->setCorrecta(false, $e->getMessage());
             return $this->resposta;
 		}
     }
     
-    public function get($id)
-    {
-        //TODO
+    public function get($id){
+        try{
+                $sql = "SELECT id_aut,nom_aut,fk_nacionalitat FROM autors WHERE ID_AUT LIKE $id";
+                $stm=$this->conn->prepare($sql);
+                $stm->execute();
+
+                $tuples=$stm->fetchAll();
+                $this->resposta->setDades($tuples);    // array de tuples
+                $this->resposta->setCorrecta(true);       // La resposta es correcta        
+                return $this->resposta;
+            }
+        catch(Exception $e){   // hi ha un error posam la resposta a fals i tornam missatge d'error
+        $this->resposta->setCorrecta(false, $e->getMessage());
+        return $this->resposta;
+            }
     }
 
     
-    public function insert($data)
-    {
-		try 
-		{
+    public function insert($data){
+		try{
                 $sql = "SELECT max(id_aut) as N from autors";
                 $stm=$this->conn->prepare($sql);
                 $stm->execute();
@@ -64,27 +70,49 @@ class Autor
        	        $this->resposta->setCorrecta(true);
                 return $this->resposta;
         }
-        catch (Exception $e) 
-		{
+        catch (Exception $e){
                 $this->resposta->setCorrecta(false, "Error insertant: ".$e->getMessage());
                 return $this->resposta;
 		}
     }   
     
-    public function update($data)
-    {
-        // TODO
+    public function update($data , $id_autor){
+        try{
+
+        $sql = "UPDATE autors SET NOM_AUT = '$data' WHERE ID_AUT LIKE $id_autor";
+        $stm=$this->conn->prepare($sql);
+        $stm->execute();
+
+        $this->resposta->setCorrecta(true);            
+        return $this->resposta;
+
+        }
+        catch (Exception $e){
+                $this->resposta->setCorrecta(false, "Error Actualitzant: ".$e->getMessage());
+                return $this->resposta;
+        }
     }
 
     
     
-    public function delete($id)
-    {
-        // TODO
+    public function delete($id){
+        try{
+
+        $sql = "DELETE FROM autors  WHERE ID_AUT LIKE $id";
+        $stm=$this->conn->prepare($sql);
+        $stm->execute();
+            
+        $this->resposta->setCorrecta(true , "S'ha borrat correctament");            
+        return $this->resposta;
+
+        }
+        catch (Exception $e){
+                $this->resposta->setCorrecta(false, "Error borrant: ".$e->getMessage());
+                return $this->resposta;
+        }
     }
 
-    public function filtra($where,$orderby,$offset,$count)
-    {
+    public function filtra($where,$orderby,$offset,$count){
         // TODO
     }
     
--- prova.php
+++ prova.php
@@ -2,7 +2,11 @@
 $base = __DIR__;
  require_once("$base/model/autor.class.php");
  $autor=new Autor();
- $res=$autor->getAll();
+//$res=$autor->getAll();
+$res=$autor->get(6777);
+//$res=$autor->update("UnAltre" , 6778 );
+//$res=$autor->delete(6777);
+
  if ($res->correcta) {
     foreach ($res->dades as $row){
         echo $row['id_aut']."-".$row['nom_aut']." ".$row["fk_nacionalitat"]."<br>";
@@ -11,9 +15,11 @@ $base = __DIR__;
      echo $res->missatge;
  }
 
- $autor->insert(array("nom_aut"=>"Tomeu Campaner","fk_nacionalitat"=>"MURERA"));   //produira un error
- if (!$res->correcta) {
-    echo "Error insertant";  // Error per l'usuari
-    error_log($res->missatge,3,"$base/log/errors.log");  // Error per noltros
- }   
+//  $autor->insert(array("nom_aut"=>"Tomeu Campaner","fk_nacionalitat"=>"MURERA"));   //produira un error
+//  if (!$res->correcta) {
+//     echo "Error insertant";  // Error per l'usuari
+//     error_log($res->missatge,3,"$base/log/errors.log");  // Error per noltros
+//  }
+ 
+
 
